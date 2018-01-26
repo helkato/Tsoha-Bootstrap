c0f117b98481e4cff6741fdc3fb2f6eed0ab81a7
kati <kati_ojala@hotmail.com>
2018-01-26 23:29:59 +0200

    tietokantataulujen luonti

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,18 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Paikka(
+tunniste SERIAL PRIMARY KEY,
+nimi varchar(50) NOT NULL,
+osoite varchar(200),
+gps varchar(30),
+kuvaus varchar(300)
+);
+
+CREATE TABLE Mittaus(
+tunniste SERIAL PRIMARY KEY,
+nimi varchar(20) NOT NULL,
+kuvaus varchar(100),
+ohje varchar(300),
+valineet varchar(200),
+ajankohta varchar(100),
+dokumentointi varchar(100)
+);
