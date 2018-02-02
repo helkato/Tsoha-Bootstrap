419e1ca260e77b9793bbe9a8ec15d061e1537c89
kati <kati_ojala@hotmail.com>
2018-02-02 15:09:44 +0200

    päivitys

--- config/routes.php
+++ config/routes.php
@@ -1,32 +1,60 @@
 <?php
 
-  $routes->get('/', function() {
-    HelloWorldController::kirjautuminen();
-  });
-  
-   $routes->get('/mittaus/1', function() {
-    HelloWorldController::mittaus();
-  });
-  
-   $routes->get('/mittaus', function() {
-    HelloWorldController::mittausluettelo();
-  });
-  
- $routes->get('/paikka/1', function() {
-    HelloWorldController::paikka();
-  });
-   $routes->get('/paikka', function() {
-    HelloWorldController::paikkaluettelo();
-  });
-  
-   $routes->get('/tulos/1', function() {
-    HelloWorldController::tulos();
-  });
-  
-  $routes->get('/tulos', function() {
-    HelloWorldController::tulosluettelo();
-  });
+// tutkimusaiheiden listaussivu
+$routes->get('/tutkimusaihe', function() {
+    TutkimusaiheController::index();
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+// Tutkimusaiheen lisätietosivu, sis luettelon tutkimuksista - EI TOIMI
+$routes->get('/tutkimusaihe/:id', function($id) {
+    TutkimusaiheController::show($id);
+});
+
+//Vesistöjen listaussivu
+$routes->get('/vesisto', function(){
+    VesistoController::index();
+});
+
+//Yksittäisen vesistöjen esittelysivu
+$routes->get('/vesisto/:id', function($id){
+    VesistoController::show($id);
+});
+
+
+
+
+
+
+
+$routes->get('/', function() {
+    HelloWorldController::kirjautuminen();
+});
+
+$routes->get('/mittaus/1', function() {
+    HelloWorldController::mittaus();
+});
+
+$routes->get('/mittaus', function() {
+    HelloWorldController::mittausluettelo();
+});
+
+$routes->get('/paikka/1', function() {
+    HelloWorldController::paikka();
+});
+$routes->get('/paikka', function() {
+    HelloWorldController::paikkaluettelo();
+});
+
+$routes->get('/tulos/1', function() {
+    HelloWorldController::tulos();
+});
+
+$routes->get('/tulos', function() {
+    HelloWorldController::tulosluettelo();
+});
+
+
+
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
