6432495b11dd11fc9bfaf747d24666848f317e74
kati <kati_ojala@hotmail.com>
2018-02-02 15:02:05 +0200

    vesistön tietojen tarkastelu

---
+++ app/models/vesisto.php
@@ -0,0 +1,52 @@
+<?php
+
+class Vesisto extends BaseModel {
+
+    // Attribuutit
+    public $id, $nimi, $tyyppi, $kuvaus;
+
+    // Konstruktori
+    public function __construct($attributes) {
+        parent::__construct($attributes);
+    }
+
+    // metodi, joka hakee kaikki veistöt ja palauttaa ne Vesisto-olioina
+    public static function all() {
+        $query = DB::connection()->prepare('SELECT * FROM Vesisto');
+        $query->execute();
+        $rows = $query->fetchAll();
+
+        $vesistot = array();
+
+        foreach ($rows as $row) {
+            $vesistot[] = new Vesisto(array(
+                'id' => $row['id'],
+                'nimi' => $row['nimi'],
+                'tyyppi' => $row['tyyppi'],
+                'kuvaus' => $row['kuvaus']
+            ));
+        }
+        return $vesistot;
+    }
+
+    // metodi yksittäisen vesiston tietojen näyttämiseksi
+    public static function find($id) {
+        $query = DB::connection()->prepare('SELECT * FROM Vesisto WHERE id = :id LIMIT 1');
+        $query->execute(array('id' => $id));
+        $row = $query->fetch();
+
+       // $vesisto = array();  // voiko taulukon luomisen jättää pois?
+
+        if ($row) {
+            $vesisto[] = new Vesisto(array(
+                'id' => $row['id'],
+                'nimi' => $row['nimi'],
+                'tyyppi' => $row['tyyppi'],
+                'kuvaus' => $row['kuvaus']
+            ));
+            return $vesisto;
+        }
+        return null;
+    }
+
+}
